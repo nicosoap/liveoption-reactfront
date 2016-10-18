8e51960896a86876fb023f7f672d013b4d1503da
Olivier PICHOU <opichou@e2r1p3.42.fr>
2016-10-18 13:34:04 +0200

    search module

--- src/search.js
+++ src/search.js
@@ -13,6 +13,7 @@ export default class Search extends Component {
     }
 
     searchSubmit = e => {
+        e.preventDefault()
         const body = e.target.value
         //this.props.simpleSearch(body)
     }
@@ -24,6 +25,30 @@ export default class Search extends Component {
                     <input className="search-input" type="search" name="search" id="search" placeholder="Search..."
                            onKeyUp={this.simpleSearch}/>
                     <button className="search-input" type="submit" name="submit" ><i className="material-icons icon-small">search</i></button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export class ExtendedSearch extends Component {
+    state = {}
+    extendedSearch = e => {
+        e.preventDefault()
+        const body = e.target.value
+        //this.props.extendedSearch(body)
+    }
+    render() {
+        return (
+            <div className="card-2 extended-search" id="extended-search">
+                <form name="extended-search" onSubmit={this.extendedSearch} >
+                    <input type="search" name="search" placeholder="Search..." />
+                    <input type="checkbox" name="around-me" id="around-me" value="true" /><label htmlFor="around-me">Around me</label>
+                    <input type="checkbox" name="chat" id="chat" /><label htmlFor="chat">Disscussion</label>
+                    <input type="checkbox" name="coffee" id="coffee" /><label htmlFor="coffee">Coffee or drinks</label>
+                    <input type="checkbox" name="lunch" id="lunch" /><label htmlFor="lunch">Lunch or diner</label>
+                    <input type="checkbox" name="netflix" id="netflix" /><label htmlFor="netflix">Netflix & chill</label>
+                    <input type="checkbox" name="right-now" id="right-now" /><label htmlFor="right-nom">Right now</label>
                 </form>
             </div>
         )
