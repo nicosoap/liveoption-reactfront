4ceeb1855db186b5d90651145779fcdff05e831c
Olivier PICHOU <opichou@e3r1p4.42.fr>
2016-10-19 18:00:30 +0200

    tag mgmt front side

--- src/taginput.js
+++ src/taginput.js
@@ -2,46 +2,22 @@
  * Created by opichou on 10/19/16.
  */
 import React, { Component } from 'react'
-import { WithContext as ReactTags } from 'react-tag-input'
 
 export default class TagInput extends Component {
     state = {
-            tags: [ {id: 1, text: "Apples"} ],
-            suggestions: ["Banana", "Mango", "Pear", "Apricot"]
-        }
-    handleDelete(i) {
-        let tags = this.state.tags
-        tags.splice(i, 1)
-        this.setState({tags})
-    }
-    handleAddition(tag) {
-        let tags = this.state.tags
-        tags.push({
-            id: tags.length + 1,
-            text: tag
-        })
-        this.setState({tags})
-    }
-    handleDrag(tag, currPos, newPos) {
-        let tags = this.state.tags
-
-        // mutate array
-        tags.splice(currPos, 1)
-        tags.splice(newPos, 0, tag)
-
-        // re-render
-        this.setState({ tags: tags }, () => this.props.changeTags(tags))
+        tags:['lol', 'lil', 'lal']
     }
     render() {
-        let { tags, suggestions } = this.state
-        return (
-            <div>
-                <ReactTags tags={tags}
-                           suggestions={suggestions}
-                           placeholder="Matching those tags"
-                           handleDelete={this.handleDelete}
-                           handleAddition={this.handleAddition}
-                           handleDrag={this.handleDrag} />
+        return(
+            <div className="input-tags" id="input-tags">
+                {this.state.tags.map((tag, i) => {
+                    return(
+                        <span className="input-tag" key={i}>
+                        {'#' + tag + ' '}<i className="material-icons icon-small lower">remove_circle</i>
+                    </span>
+                    )
+                })}
+                <input className="input-tags-search" type="search" />
             </div>
         )
     }
